792c71be48bcf3492e96fe77fa2fd4bbea796479
Audrey Castillo <valax321@gmail.com>
2026-09-21 16:17:38 +1000

    Add texture building example content

--- source/examples/Stageplay.Example.ContentBuilder/BuilderRecipe.cs
+++ source/examples/Stageplay.Example.ContentBuilder/BuilderRecipe.cs
@@ -1,20 +1,28 @@
 using CommandLine;
 using Radish.ContentBuilder;
-using Radish.Lua;
+using Radish.ContentBuilder.StandardAssetProcessors;
 
 namespace Radish;
 
-public class StageplayRecipe : ICookRecipe
+public class StageplayExampleRecipe : ICookRecipe
 {
     // We promise, .NET just won't let me use required in interfaces for some reason.
     public BuilderTask Task { get; init; } = null!;
     
     public void BuildProcessorQueue(ProcessorQueue queue)
     {
+        // The runtime expects all Lua scripts to be in the scripts/ folder, so only look there.
         queue.AddByGlobPattern("scripts/**/*.lua", 
-            new LuaCompilerProcessor());
+            new LuaBytecodeProcessor());
+        
+        // Images are allowed anywhere in the content path.
+        // Handle some common formats.
+        queue.AddByGlobPattern([
+            "**/*.png",
+            "**/*.jpg"
+        ], new TextureProcessor());
     }
 }
 
 [Verb("build", HelpText = "Builds all assets for the game.")]
-public class MonoGameCookTask : CookTask<StageplayRecipe>;
+public class ExampleCookTask : CookTask<StageplayExampleRecipe>;
